3d5533b6dab11f02be9c478757b223be1892c57f
chonlele <anh.dt2605@gmail.com>
2024-12-03 22:03:38 +0700

    [Update] Add printing through internet

--- examples/printing_usb/main.go
+++ examples/printing_usb/main.go
@@ -12,10 +12,18 @@ func main() {
 	// Please keep in mind that option layout have not supported yet
 	layoutPath := "data/layout/layout.txt"
 
-	// Printing with template data
-	pos.PrintFromTemplate(templatePath, layoutPath)
+	// Init pos handler with template path
+	posHandlerTemplate, err := pos.NewPosFromTemplateFile(templatePath, layoutPath)
+	if err != nil {
+		panic(err)
+	}
 
-	// Printing with object data
+	// Printing
+	// USBPath empty which mean using default usb printing gate
+	posHandlerTemplate.PrintWithUSB("")
+
+	// ----------------------------------------------------------------------------------------
+	// Printing with ticket object data
 	ticket := model.Ticket{
 		Index:       "1",
 		Terminal:    "CashOut",
@@ -88,5 +96,10 @@ func main() {
 		},
 	}
 
-	pos.PrintFromTicket(ticket, layoutPath)
+	// Init Handler with ticket object
+	posHandler := pos.NewPos(ticket, layoutPath)
+
+	// Printing
+	// Path empty which mean using default usb printing gate
+	posHandler.PrintWithUSB("")
 }
